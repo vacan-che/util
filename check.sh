a1e840e1eb6860dafcb65677e99a2134e296385c
vacan-che <38682140+vacan-che@users.noreply.github.com>
2023-03-31 19:33:40 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-
 network="192.168.100"
 for addr in {1..255}; do
     ping -c 1 -W 1 "${network}.${addr}" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "192.168.100.${addr} is up"
+    else 
+        echo "192.168.100.${addr} is down"
     fi
 done

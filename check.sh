ba5ad3601362fae63bec65b0d43969d9d6caf589
vacan-che <38682140+vacan-che@users.noreply.github.com>
2023-03-31 19:44:16 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,9 +1,8 @@
 network="192.168.100"
-for addr in {100..200}; do
+for addr in {1..255}; do
     ping -c 1 -W 1 "${network}.${addr}" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "192.168.100.${addr} is up"
-    else 
-        echo "192.168.100.${addr} is down"
+        mac=$(arp "${network}.${addr}" | awk '{print $3}')
+        echo "${network}.${addr} is up (MAC address: ${mac})"
     fi
 done

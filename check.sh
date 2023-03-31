eb55772f31b3f64cd7356a87831e0ee46bda5bb4
vacan-che <38682140+vacan-che@users.noreply.github.com>
2023-03-31 19:52:50 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@ network="192.168.100"
 for addr in {1..255}; do
     ping -c 1 -W 1 "${network}.${addr}" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
-        mac=$(arp "${network}.${addr}" | awk '{print $3}')
+        mac=$(arp -a "${network}.${addr}" | awk '{print $4}')
         echo "${network}.${addr} is up (MAC address: ${mac})"
     fi
 done

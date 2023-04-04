0b865d97a836413a16a25fc73ae25119dcff5166
vacan-che <38682140+vacan-che@users.noreply.github.com>
2023-04-04 14:22:21 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 network="192.168.100"
-for addr in {1..255}; do
+for addr in {100..255}; do
     ping -c 2 -W 1 "${network}.${addr}" > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         mac=$(arp -a "${network}.${addr}" | awk '{print $4}')
